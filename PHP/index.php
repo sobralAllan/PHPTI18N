5b5e4493ece79e1b5edb71a9bc5c27b10aab7e0a
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-07-17 22:08:24 -0300

    Primeiro Commit

---
+++ PHP/index.php
@@ -0,0 +1,81 @@
+<?php
+    $num1 = 0;
+    $num2 = 3;
+
+    function somar($num1,$num2){
+        return $num1 + $num2;
+    }//fim do somar
+
+    function subtrair($num1, $num2){
+        return $num1 - $num2;
+    }//fim do subtrair
+
+    function multiplicar($num1, $num2){
+        return $num1 * $num2;
+    }//fim do multiplicar
+
+    function dividir($num1,$num2){
+        if($num2 <= 0){
+            return "Impossível dividir por zero!";
+        }else{
+            return $num1 / $num2;
+        }
+    }//fim do dividir
+
+    function escolher($num1){
+        switch($num1){
+            case 1:
+                return "Rosa";
+                break;
+            case 2: 
+                return "Preto";
+                break;
+            case 3: 
+                return "White";
+                break;
+            default:
+                return "Cor não identificada";
+                break;
+        }//fim do switch
+    }//fim do escolher
+
+    function converterBinario($decimal){
+        $binario = "";
+        while($decimal >= 1){
+            $binario .= $decimal % 2;
+            $decimal = $decimal / 2;
+        }
+        return strrev($binario);
+    }//fim da conversão
+
+    function converterDecimal($binario){
+        $tamanho = strlen($binario);
+        $j = $tamanho - 1;
+        $i = 0;
+        $decimal = 0;
+        while($i < $tamanho){
+            if(substr($binario,$i,1) == "1"){
+                $decimal += pow(2,$j);
+            }
+            $j--;
+            $i++; 
+        }
+        return $decimal;
+    }//fim da conversão de binário para decimal
+
+
+
+    //Imprimir dos dados na tela
+    echo "<br>A soma dos números é: ".somar(5,6);
+    echo "<br>A subtração dos números é: ".subtrair(5,6);
+    echo "<br>A multiplicação dos números é: ".multiplicar(5,6);
+    echo "<br>A divisão dos números é: ".dividir(6,7);
+    echo "<br>A escolha é: ".escolher(2);
+    echo "<br>A conversão é: ".converterBinario(42);
+    echo "<br>A conversão de binário para decimal é: ".converterDecimal("101010");
+
+
+
+
+
+?>
