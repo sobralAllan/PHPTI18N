532609b0064c5a211704743e260210d950887179
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-07-18 22:23:57 -0300

    Segundo commit

--- PHP/index.php
+++ PHP/index.php
@@ -56,13 +56,83 @@
         while($i < $tamanho){
             if(substr($binario,$i,1) == "1"){
                 $decimal += pow(2,$j);
-            }
+            }                                                                                                                
             $j--;
             $i++; 
         }
         return $decimal;
     }//fim da conversão de binário para decimal
 
+    function converterDecimalHexa($decimal){
+        $hexa = "";
+        $resto = 0;
+        while($decimal >= 1){
+            $resto   = $decimal % 16;
+            $decimal = $decimal / 16;
+
+            switch($resto){
+                case 10:
+                    $hexa .= "A";
+                    break;
+                case 11:
+                    $hexa .= "B";
+                    break;
+                case 12:
+                    $hexa .= "C";
+                    break;
+                case 13:
+                    $hexa .= "D";
+                    break;
+                case 14:
+                    $hexa .= "E";
+                    break;
+                case 15:
+                    $hexa .= "F";
+                    break;
+                default:
+                    $hexa .= $resto;
+                    break;
+            }//fim do switch
+        }//fim do while
+
+        return strrev($hexa);
+    }//fim do método
+
+    function converterHexaDecimal($hexa){
+        $tamanho = strlen($hexa) - 1;
+        $j = $tamanho;
+        $i = 0;
+        $num = "";
+        $decimal = 0;
+        while($i <= $tamanho){
+            $num = substr($hexa,$i,1);
+            switch($num){
+                case "A":
+                    $num = 10;
+                    break;
+                case "B":
+                    $num = 11;
+                    break;
+                case "C":
+                    $num = 12;
+                    break;
+                case "D":
+                    $num = 13;
+                    break;
+                case "E":
+                    $num = 14;
+                    break;
+                case "F":
+                    $num = 15;
+                    break; 
+            }
+            
+            $decimal += $num * pow(16,$j);
+            $j--;
+            $i++;
+        }
+        return $decimal;
+    }//fim do método
 
 
     //Imprimir dos dados na tela
@@ -73,8 +143,8 @@
     echo "<br>A escolha é: ".escolher(2);
     echo "<br>A conversão é: ".converterBinario(42);
     echo "<br>A conversão de binário para decimal é: ".converterDecimal("101010");
-
-
+    echo "<br>A conversão de decimal para hexa é: ".converterDecimalHexa(1000);
+    echo "<br>A conversão de hexa para decimal é: ".converterHexaDecimal("3E8");
 
 
 
